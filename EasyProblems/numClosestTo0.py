ba48d2667e2c7eecc3e2e698f68e1637c20970df
abdullah-zzz <ak0344494@gmail.com>
2024-10-05 20:07:52 +0500

    solved an easy question

--- EasyProblems/numClosestTo0.py
+++ EasyProblems/numClosestTo0.py
@@ -10,14 +10,14 @@ class Solution:
         for i in range(1,len(nums)):
             # abs() gives us the abosolute value. It basically changes a negative number into a positive number.
             # consider this list [-4,2,-5,6], we turn all the number to positive to easily check which one is closer to 0 
-            # we check wheather the abs value of number is less than the abs value of the number stored in the closest variable. If it is smaller then we update closest
+            # we check wheather the abs value of the current number is less than the abs value of the number stored in the closest variable. If it is smaller then we update closest
             # to the smaller number
             if abs(nums[i]) < abs(closest):
                 closest = nums[i]
 
             # the question also said to return the greater number in case there were two numbers close to 0. for example: -1,1
             # one is negative and one is postive but both of them are the same times apart from 0 
-            # we the numbers to positive number and then check wheather they are equal if they are we update closest with the greater number
+            # we change the numbers to positive number and then check wheather they are equal if they are we update closest with the greater number
             elif abs(nums[i]) == abs(closest):
                 if abs(nums[i]) > closest:
                     closest = nums[i]
